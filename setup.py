4b33112f55c105ab417660933c1d1044a27ac90e
JashJummani <jash.logicwind@gmail.com>
2025-01-08 10:11:55 +0530

    Structure added for the project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Jash Jummani",
+    author_email="jummanijash.77@gmail.com",
+    packages=find_packages()
+)
